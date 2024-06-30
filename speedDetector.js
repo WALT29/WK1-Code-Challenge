a4e6db8a4a8b2e13dbca8a026739c9aa61804413
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-06-30 20:59:28 +0300

    WEEK 1 CODE CHALLENGE

---
+++ speedDetector.js
@@ -0,0 +1,36 @@
+let speedLimit=70;
+let kmDemirit=5;
+
+
+function speedRegulator(){
+    let speed =prompt('ENTER YOUR DRIVING SPEED');
+    speed=Number(speed);
+
+    //we validate the speed
+    if(isNaN(speed) || speed<0){
+        console.log('ENTER A VALID SPEED');
+        alert('ENTER A VALID SPEED')
+        return;
+    }
+    //after validating the speed we evaluate the speed based on the given conditions
+    if (speed<=speedLimit){
+        console.log('OK');
+        alert('OK')
+    }
+    else{
+        demeritCalculator(speed);
+    }
+
+}
+function demeritCalculator(speed){
+    let exceededSpeed=speed-speedLimit;
+    let demeritPoints=Math.floor(exceededSpeed/kmDemirit);
+    if(demeritPoints<12){
+        console.log(`You have the following demerit points : ${demeritPoints}`);
+        alert(`You have the following demerit points : ${demeritPoints}`);
+    }else{
+        console.log(`LICENCE SUSPENDED ,DEMERIT POINTS: ${demeritPoints}`);
+    }
+}
+
+speedRegulator();
